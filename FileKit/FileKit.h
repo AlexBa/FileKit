b1b8934ed8d41389e738ad96a41f324479b0ed4e
GoodCrypt <admin@goodcrypt.com>
2015-02-01 13:09:41 +0100

    Created a basic project

---
+++ FileKit/FileKit.h
@@ -0,0 +1,19 @@
+//
+//  FileKit.h
+//  FileKit
+//
+//  Created by Alexander Barton on 01.02.15.
+//  Copyright (c) 2015 CipherBits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FileKit.
+FOUNDATION_EXPORT double FileKitVersionNumber;
+
+//! Project version string for FileKit.
+FOUNDATION_EXPORT const unsigned char FileKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FileKit/PublicHeader.h>
+
+
